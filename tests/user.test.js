26ed24ed49ff60aa8be200080e24e2482c87bcb5
zeeshan12312 <zeeshan.mohammad@gmail.com>
2022-09-09 17:34:56 +0500

    unit test added

---
+++ tests/user.test.js
@@ -0,0 +1,20 @@
+const user = require('../controllers/usersController');
+
+describe('User Accounts', () => {
+  it('List', () => {
+    const result = user.userslist();
+    expect(result).toBeDefined();
+  });
+  it('Get User By Id', () => {
+    const result = user.updateUserById('62f38751d9c2a83b18d90e8d');
+    expect(result).toMatchObject({});
+  });
+  it('Add User', () => {
+    const args = [null, '', NaN, 0, false, undefined];
+    args.forEach((a) => {
+      expect(() => {
+        user.addUser(a);
+      }).toThrow();
+    });
+  });
+});
